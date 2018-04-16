b4566d23cdfbb38ee8149f04274f015dfe8299e2
bruvecha <bruvecha@gmail.com>
2018-04-16 16:16:10 -0400

    nav agregados

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,3 +1,10 @@
-/**
- * Created by minedu on 16/04/2018.
- */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "70%";
+    // document.getElementById("flipkart-navbar").style.width = "50%";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.body.style.backgroundColor = "rgba(0,0,0,0)";
+}
